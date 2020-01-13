bad051aa0898b0b1d2cdf7a0e0f542f358d7a8ff
Jeffrey Martinez <martinezjf2@gmail.com>
2020-01-13 05:20:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,4 +8,18 @@ class School
     @roster = {}
   end
   
+  def add_student(name, grade)
+    if roster.has_key?(grade)
+    roster[grade] << name
+    else
+    roster[grade] = [name]
+   end
+end
+    
+  
+  
+  
+  
+  
+  
 end
